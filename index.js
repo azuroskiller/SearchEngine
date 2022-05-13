499d3748b1187a04ba935d0f996a84facfcf4d73
Wave <wafiqhey@gmail.com>
2022-05-13 18:25:28 +0800

    INDEX UPDATE

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ const fs = require("fs")
 
 
 async function start (){
-    const writeStream = fs.createWriteStream('txt/jobs.txt');
-    const pathName = writeStream.path;
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage();
 
-    var name = ["Job Name"];
-    var country,company,type,salary,skills,desc,req,resp,industry = [];
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null
+    });
+    
 
-    for (var j=1; j<3; j++){
-        await page.goto("https://startupjobs.asia/job/search?q=&job-list-dpl-page="+j, {timeout: 3000000})
+    var name     = ["Job Name"];
+    var country  = ["Country"];
+    var company  = ["Company Name"];
+    var type     = ["Job Type"];
+    var salary   = ["Salary"];
+    var skills   = ["Skills Require"];
+    var desc     = ["Job Description"];
+    var req      = ["Job Requirements"];
+    var resp     = ["Job Responsibility"];
+    var industry = ["Industry"];
+
+    for (var j=1; j<5; j++){
+
+        const page = await browser.newPage();
+        page.setDefaultNavigationTimeout( 0 );
+        
+        await page.goto('https://startupjobs.asia/job/search?q=&job-list-dpl-page='+j, {
+            waitUntil: "load",
+            timeout: 0,
+        });
 
         for ( var i=1; i < 31; i++){
 
             await page.waitForXPath("/html/body/div[1]/div[3]/div[1]/div/div[1]/ul/li["+i+"]/div/div[1]/div/h5/a")
             var b = await page.$x("/html/body/div[1]/div[3]/div[1]/div/div[1]/ul/li["+i+"]/div/div[1]/div/h5/a")
-            await b[0].click()
-
+            await b[0].click();
+            
             const elementsToFind = [
                 { xpath: "/html/body/div[1]/div[3]/div[1]/div/div[1]/ul/li["+i+"]/div/div[1]/div/h5/a",  propName: 'job_name' },
                 { xpath: '/html/body/div[1]/div[3]/div[2]/div[1]/div[2]/div/h6[2]/a',                    propName: 'country' },      
@@ -49,13 +66,31 @@ async function start (){
             }    
 
             name.push(results['job_name']);
+            country.push(results['country']);
+            company.push(results['company']);
+            type.push(results['job_type']);
+            salary.push(results['salary']);
+            skills.push(results['skills']);
+            desc.push(results['job_description']);
+            req.push(results['job_requirement']);
+            resp.push(results['job_responsibility']);
+            industry.push(results['industry']);
 
             //await page.evaluate(() => document.querySelector("#suj-single-jobdetail-wrapper > div.detail-body > div.row > div.col.s12.tabs-wrapper.suj-company-review-tabs-wrapper > ul > li:nth-child(2) > a").click())            
         }
+        await page.close();
     }
-    console.log(name[10]);
+
+    var k=1;
+    name.forEach(function(a, index) {
+        console.log(k, a);
+        k++;
+    });
+
+    console.log(company[10])
     
-    await browser.close()
+  
+    await browser.close();
 }
 
 start()
