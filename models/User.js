3c3a8505ba995c9501fc07438a4b29c3497e21fb
NxTech4021 <wafiqhey@gmail.com>
2022-08-03 18:51:57 +0800

    test

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const moment = require('moment-timezone')
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://hairul:hairul123@crawler1.u1w6t.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
+
+mongoose.connection
+    .once('open', () => console.log('Connected'))
+    .on("error", error => {
+        console.log("Your Error", error);
+    });
+
+const UserSchema = new mongoose.Schema({
+
+    "userID": {
+        type: Number
+    },
+    "email": {
+        type: String,
+        required: true
+    },
+    "name": {
+        type: String,
+        required: true
+    },
+    "password": {
+        type: String,
+        required: true
+    },
+    "postition": {
+        type: String,
+        required: true
+    },
+    "createdOn": {
+        type: String,
+        default: () => moment().utcOffset(+480).format()
+    },
+    "userLevel": {
+        type: Int32,
+        default: '0'
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    "confirmToken": {
+        type: String,
+        default: ''
+    },
+    "verifier": {
+        type: Boolean,
+        default: false
+    },
+})
+
+module.exports = mongoose.model('WebCrawler', UserSchema)
